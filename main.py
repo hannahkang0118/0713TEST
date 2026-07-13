3bf4708c83f727d85e36b260cce586813cbb8b48
hannahkang0118 <hannahkangmmm@gmail.com>
2026-07-13 10:43:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
  
-st.title("나의 첫 웹앱")
-st.write("안녕하세요! 반갑습니다.")
-지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산"])
+st.title("앱 by HN")
+st.write("반갑습니다!")
+지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산", "전주", "제주"])
 st.write("당신이 고른 지역:", 지역)
 숫자 = st.slider("좋아하는 숫자", 0, 100)
 st.write("고른 숫자:", 숫자)
